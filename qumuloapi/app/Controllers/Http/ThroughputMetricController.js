51d49c4d75ef14fd0aced8deb0ed3ccc52580abe
simarsaini17 <gursimrat2352@gmail.com>
2024-04-10 17:48:01 -0600

    Backend setup

---
+++ qumuloapi/app/Controllers/Http/ThroughputMetricController.js
@@ -0,0 +1,92 @@
+"use strict";
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with throughputmetrics
+ */
+
+const ThroughputData = use("App/Models/ThroughputMetric");
+
+class ThroughputMetricController {
+  /**
+   * Show a list of all throughputmetrics.
+   * GET throughputmetrics
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index({ request, response, view }) {
+    const data = await ThroughputData.all();
+    return response.json(data);
+  }
+
+  /**
+   * Render a form to be used for creating a new throughputmetric.
+   * GET throughputmetrics/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create({ request, response, view }) {}
+
+  /**
+   * Create/save a new throughputmetric.
+   * POST throughputmetrics
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store({ request, response }) {}
+
+  /**
+   * Display a single throughputmetric.
+   * GET throughputmetrics/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show({ params, request, response, view }) {}
+
+  /**
+   * Render a form to update an existing throughputmetric.
+   * GET throughputmetrics/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit({ params, request, response, view }) {}
+
+  /**
+   * Update throughputmetric details.
+   * PUT or PATCH throughputmetrics/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update({ params, request, response }) {}
+
+  /**
+   * Delete a throughputmetric with id.
+   * DELETE throughputmetrics/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy({ params, request, response }) {}
+}
+
+module.exports = ThroughputMetricController;
